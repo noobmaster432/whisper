6df5978f8417d732de991de063015cded117807b
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-08-05 00:56:41 +0530

    search messages done

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -5,9 +5,10 @@ import { IoVideocam } from "react-icons/io5";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
 
 function ChatHeader() {
-  const [{currentChat}] = useStateProvider();
+  const [{currentChat}, dispatch] = useStateProvider();
   return (
     <div className="h-16 px-4 py-3 flex justify-between items-center bg-panel-header-background z-10">
       <div className="flex items-center justify-center gap-4">
@@ -20,7 +21,7 @@ function ChatHeader() {
       <div className="flex gap-6">
         <MdCall className="text-panel-header-icon text-xl cursor-pointer" />
         <IoVideocam className="text-panel-header-icon text-xl cursor-pointer" />
-        <BiSearchAlt2 className="text-panel-header-icon text-xl cursor-pointer" />
+        <BiSearchAlt2 className="text-panel-header-icon text-xl cursor-pointer" onClick={() => dispatch({ type: reducerCases.SET_MESSAGE_SEARCH })} />
         <BsThreeDotsVertical className="text-panel-header-icon text-xl cursor-pointer" />
       </div>
     </div>
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -10,25 +10,35 @@ import { CHECK_USER_ROUTE, GET_MESSAGES_ROUTE, HOST } from "@/utils/ApiRoutes";
 import { reducerCases } from "@/context/constants";
 import Chat from "./Chat/Chat";
 import { io } from "socket.io-client";
+import SearchMessages from "./Chat/SearchMessages";
 
 function Main() {
   const router = useRouter();
-  const [{ userInfo, currentChat }, dispatch] = useStateProvider();
+  const [{ userInfo, currentChat, messagesSearch }, dispatch] =
+    useStateProvider();
   const [redirectLogin, setRedirectLogin] = useState(false);
   const [socketEvent, setSocketEvent] = useState(false);
   const socket = useRef();
 
   useEffect(() => {
     if (redirectLogin) router.push("/login");
-  },[redirectLogin]);
+  }, [redirectLogin]);
 
   onAuthStateChanged(firebaseAuth, async (currentUser) => {
     if (!currentUser) setRedirectLogin(true);
     if (!userInfo && currentUser?.email) {
-      const { data } = await axios.post(CHECK_USER_ROUTE, { email: currentUser.email });
+      const { data } = await axios.post(CHECK_USER_ROUTE, {
+        email: currentUser.email,
+      });
       if (!data.status) router.push("/login");
       if (data?.data) {
-        const { id, name, email, profilePicture: profileImage, status } = data.data;
+        const {
+          id,
+          name,
+          email,
+          profilePicture: profileImage,
+          status,
+        } = data.data;
         dispatch({
           type: reducerCases.SET_USER_INFO,
           userInfo: {
@@ -49,30 +59,44 @@ function Main() {
       socket.current.emit("add-user", userInfo.id);
       dispatch({ type: reducerCases.SET_SOCKET, socket });
     }
-  },[userInfo]);
+  }, [userInfo]);
 
   useEffect(() => {
     if (socket.current && !socketEvent) {
       socket.current.on("msg-receive", (data) => {
-        dispatch({ type: reducerCases.ADD_MESSAGE, newMessage: { ...data.message }});
+        dispatch({
+          type: reducerCases.ADD_MESSAGE,
+          newMessage: { ...data.message },
+        });
       });
       setSocketEvent(true);
     }
-  },[socket.current]);
+  }, [socket.current]);
 
   useEffect(() => {
-    const getMessages = async() => {
-      const { data: { messages } } = await axios.get(`${GET_MESSAGES_ROUTE}/${userInfo.id}/${currentChat.id}`);
+    const getMessages = async () => {
+      const {
+        data: { messages },
+      } = await axios.get(
+        `${GET_MESSAGES_ROUTE}/${userInfo.id}/${currentChat.id}`
+      );
       dispatch({ type: reducerCases.SET_MESSAGES, messages });
-    }
+    };
     if (currentChat?.id) getMessages();
-  },[currentChat]);
+  }, [currentChat]);
 
   return (
     <>
       <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
         <ChatList />
-        {currentChat ? <Chat /> : <Empty />}
+        {currentChat ? (
+          <div className={messagesSearch && "grid grid-cols-2"}>
+            <Chat />
+            {messagesSearch && <SearchMessages /> }
+          </div>
+        ) : (
+          <Empty />
+        )}
       </div>
     </>
   );
